7e80869900565a2b17656fc0c7b1b270b166c35c
Yuns3mre <acaryunusemre99@gmail.com>
2023-10-15 22:59:49 +0300

    US_001_PositionManage

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -1,22 +1,52 @@
 package pages;
 
 import drivers.Driver;
+import helpers.SeleniumShortcuts;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-public class LoginPage {
+public class LoginPage extends SeleniumShortcuts {
 
     public LoginPage() {
         PageFactory.initElements(Driver.getDriver(), this);
     }
 
-    @FindBy(id="[id='mat-input-3']")
+    @FindBy(xpath = "//input[@placeholder='Kullanıcı Adı']")
     public WebElement username;
 
-    @FindBy(id="[id='mat-input-4']")
+    @FindBy(xpath="//input[@placeholder='Parola']")
     public WebElement password;
 
-    @FindBy(linkText="GİRİŞ YAP")
+    @FindBy(xpath = "//span[@class='mdc-button__label']")
     public WebElement login;
+
+    @FindBy(xpath="//ms-add-button[contains(@tooltip,'ADD')]//button")
+    public WebElement addButton;
+
+    @FindBy(xpath="//ms-text-field[@formcontrolname='name']//input")
+    public WebElement nameInput;
+
+    @FindBy(xpath="//ms-save-button/button")
+    public WebElement saveButton;
+
+    @FindBy(css="span[class='mat-mdc-tooltip-trigger logo-text']")
+    public WebElement txtTechnoStudy;
+
+    @FindBy(xpath="//div[contains(text(),'successfully')]")
+    public WebElement successMessage;
+
+    @FindBy(xpath="//input[@id='ms-text-field-0']")
+    public WebElement searchBtn;
+
+    @FindBy(xpath="//ms-search-button//button")
+    public WebElement searchPicBtn;
+
+    @FindBy(xpath="(//ms-delete-button//button)[1]")
+    public WebElement deletePicBtn;
+
+    @FindBy(xpath="//button[@type='submit']")
+    public WebElement deleteBtn;
+
+
 }
---
+++ src/test/java/stepDefs/US_001_PositionManage.java
@@ -0,0 +1,68 @@
+package stepDefs;
+
+import drivers.Driver;
+import io.cucumber.java.en.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import pages.LoginPage;
+import pages.NavbarPage;
+
+public class US_001_PositionManage {
+
+    LoginPage lp=new LoginPage();
+    NavbarPage np=new NavbarPage();
+
+    @Given("Navigate to Campus")
+    public void navigateToCampus() {
+        Driver.getDriver().get("https://test.mersys.io/");
+    }
+
+    @When("Enter username and password and click login button")
+    public void LoginStuff() {
+        lp.sendKeys(lp.username, "turkeyts");
+        lp.sendKeys(lp.password, "TechnoStudy123");
+        lp.click(lp.login);
+    }
+
+    @Then("User should login successfully")
+    public void loginSuccessfully() {
+        lp.checkTextPresence(lp.txtTechnoStudy,"Techno Study");
+
+    }
+
+    @And("Navigate to Position Categories")
+    public void createCategori(){
+        np.click(np.humanResou);
+        np.click(np.Setup);
+        np.wait.until(ExpectedConditions.elementToBeClickable(np.positionCate));
+        np.click(np.positionCate);
+    }
+
+    String newPosition ="Evulation";
+    @When("Create a New Position Category")
+    public void addPosiCategory(){
+
+
+        lp.click(lp.addButton);
+        lp.sendKeys(lp.nameInput, newPosition);
+        lp.click(lp.saveButton);
+    }
+
+    @Then ("Success message should be displayed")
+    public void succesMessage(){
+        lp.checkTextPresence(lp.successMessage,"successfully");
+    }
+
+
+    @When("User delete the {string}")
+    public void userDeleteThe(String name) {
+        lp.click(lp.searchBtn);
+        lp.sendKeys(lp.searchBtn,newPosition);
+        lp.click(lp.searchPicBtn);
+        lp.wait.until(ExpectedConditions.numberOfElementsToBe(By.xpath("//fuse-progress-bar/*"),0));
+        lp.click(lp.deletePicBtn);
+        lp.click(lp.deleteBtn);
+
+
+    }
+}
